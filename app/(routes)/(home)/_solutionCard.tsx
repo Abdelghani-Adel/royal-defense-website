7441a4267c92bbdcd34fc0e3b8a3d2d9b8abcebb
Abdelghani_Adel <abdelghani.private@yahoo.com>
2024-03-29 06:39:57 +0200

    cards data

--- app/(routes)/(home)/_solutionCard.tsx
+++ app/(routes)/(home)/_solutionCard.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const SolutionCard = ({ data }: { data: ISolutionCardData }) => {
+  const { img, title, desc, urlTarget } = data;
+
   return (
     <div className="solutionCard">
       <div className="solutionCard_image">
-        <Image src="/images/solutionscards/camera2.webp" fill alt="" />
+        <Image src={img} fill alt="" />
       </div>
-
-      <h5 className="textPrimary text-center">Video Surveillance systems</h5>
-
-      <p className="fw-light">
-        Our wealth of experience encompasses the design and integration of
-        highly advanced and intelligent video surveillance systems.
-      </p>
-
-      <button className="rdBtn mainBtn smBtn w-100">DISCOVER MORE</button>
+      <h5 className="textPrimary text-center">{title}</h5>
+      <p className="fw-light">{desc}</p>
+      <button className="rdBtn mainBtn smBtn w-100">
+        <Link href={urlTarget}>DISCOVER MORE</Link>
+      </button>
     </div>
   );
 };
